72080a90499d7ed4590edc40b4569744a8911fd8
Luigiguy25 <163463512+Luigiguy25@users.noreply.github.com>
2024-03-21 19:44:53 -0700

    Update and rename Shooter.cs to NewRayShooter.cs

--- Assets/Scripts/NewRayShooter.cs
+++ Assets/Scripts/NewRayShooter.cs
@@ -6,9 +6,13 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject pfBulletProjectile;
     [SerializeField]  GameObject pfBall;
+    [SerializeField]  Transform projectileSpawner1;
+    [SerializeField]  Transform projectileSpawner2;
+
     [SerializeField] public GameObject Gun1;
     [SerializeField] public GameObject Gun2;
 
+    private Camera cam;
     private GameObject bullet;
     private GameObject ball;
     int gun;
@@ -17,6 +21,7 @@ public class Shooter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        cam = GetComponent<Camera>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         gun = 1;
@@ -45,31 +50,48 @@ public class Shooter : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0))
             {
-                bullet = Instantiate(pfBulletProjectile) as GameObject;
-                bullet.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
-                bullet.transform.rotation = transform.rotation;
-                StartCoroutine(Cooldown1());
+                Vector3 point = new Vector3(cam.pixelWidth/2, cam.pixelHeight/2, 0);
+
+                Ray ray = cam.ScreenPointToRay(point);
+
+                RaycastHit hit;
+                if (Physics.Raycast(ray,out hit))
+                {
+                    StartCoroutine(Fire1());
+                }
             }
         }
         else if (gun == 2)
         {
             if (Input.GetMouseButton(0))
             {
-                ball = Instantiate(pfBall) as GameObject;
-                ball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
-                ball.transform.rotation = transform.rotation;
-                StartCoroutine(Cooldown2());
+                Vector3 point = new Vector3(cam.pixelWidth/2, cam.pixelHeight/2, 0);
+
+                Ray ray = cam.ScreenPointToRay(point);
+
+                RaycastHit hit;
+                if (Physics.Raycast(ray,out hit))
+                {
+                    StartCoroutine(Fire2());
+                }
             }
         }
+    
     }
     
-    private IEnumerator Cooldown1()
+    private IEnumerator Fire1()
     {
+        bullet = Instantiate(pfBulletProjectile, projectileSpawner1) as GameObject;
+        bullet.transform.rotation = transform.rotation;
+
         yield return new WaitForSeconds(5);
         Destroy(bullet);
     }
-    private IEnumerator Cooldown2()
+    private IEnumerator Fire2()
     {
+        ball = Instantiate(pfBall, projectileSpawner2) as GameObject;
+        ball.transform.rotation = transform.rotation;
+
         yield return new WaitForSeconds(5);
         Destroy(ball);
     }
